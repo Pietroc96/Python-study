70108a79e786d1649084e18159b0691b91773289
Pietroc96 <97255284+Pietroc96@users.noreply.github.com>
2026-03-26 09:53:37 +0100

    day 5

---
+++ Day 5/quiz_brain.py
@@ -0,0 +1,9 @@
+class Quizbrain:
+    def __init__(self, question_list):
+        question_number = 0
+        question_list = question_list
+
+    def next_question(self,question_number, question_list):
+        question_number += 1
+        question_list[question_number]
+
